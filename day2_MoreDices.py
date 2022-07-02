fe103442e286f0f7a9b09c3a4d6fab19c6000fe9
Kavita Khatavkar <66661758+kavita1910@users.noreply.github.com>
2022-07-02 20:16:28 +0530

    Add files via upload

---
+++ day2_MoreDices.py
@@ -0,0 +1,14 @@
+dice1 = [1,2,3,4,5,6]
+dice2 = [1,2,3,4,5,6]
+total = len(dice1) * len(dice2)
+total_possibilities = 0
+
+# Verify possibilities
+for i in range(len(dice1)):
+    for j in range(len(dice2)):
+        if dice1[i] != dice1[j] and (dice1[i] + dice1[j]) == 6:
+            total_possibilities = total_possibilities + 1
+            
+# Get probability
+probability = total_possibilities / total
+print(f"Probability: {total_possibilities} / {total} = {probability}")
