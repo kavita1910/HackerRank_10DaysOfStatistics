cf3cb20a5ea4c016a5e59752d9793503906b432f
Kavita Khatavkar <66661758+kavita1910@users.noreply.github.com>
2022-07-05 00:57:49 +0530

    day-5 uploads

--- day5_PossionDistribution-2.py
+++ day5_PossionDistribution-2.py
@@ -2,5 +2,6 @@ means = list(map(float, input().split()))
 mean_a = means[0]
 mean_b = means[1]
 
+# print output
 print (round(160 + 40 * (mean_a + mean_a ** 2), 3))
 print (round(128 + 40 * (mean_b + mean_b ** 2), 3))
